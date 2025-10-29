2ca6a400a28763018043caa774afb740d7f3fb48
DannyN126 <00027822@uca.edu.sv>
2025-10-29 14:47:24 -0600

    front

--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h1>🎮 Bienvenido a la Tienda de Videojuegos</h1>";
-echo "<p>Servidor Apache y PHP funcionando correctamente desde XAMPP.</p>";
-?>
--- public/test_db.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/db.php';
-echo "<h2>✅ Conexión establecida correctamente con la base de datos.</h2>";
-?>
